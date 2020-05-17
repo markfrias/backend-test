10abf12a6514ee989916a5137e8ec0c4520becab
Mark Jabel Frias <friasmarkjabel@gmail.com>
2020-05-18 01:22:41 +0800

    3

--- index.js
+++ index.js
@@ -2,30 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const formidable = require('formidable');
 const passport = require('passport');
-const multer = require('multer');
-const download = require('image-downloader');
-
 const _port = process.env.PORT || 3300;
-const options = {
-    url: "",
-    dest: './uploads/'
-}
-let filename;
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads')
-    },
-    filename: function(req, file, cb) {
-        console.log(file);
-        cb(null, filename = file.fieldname + Date.now() + file.originalname);
-        console.log(filename);
-    }
-});
-
-const upload = multer({ storage: storage });
-
-
 const cors = require('cors');
 require('./config/config');
 require('./db');
@@ -35,7 +12,6 @@ const fileHandler = require('./upload');
 
 
 const app = express();
-app.use('/uploads', express.static('uploads'));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -60,7 +36,7 @@ app.use((err, req, res, next) => {
 
 
 
-
+app.use('/uploads', express.static('uploads'));
 
 app.listen(_port, (err) => {
     if (err) { throw err; }
@@ -70,36 +46,5 @@ app.listen(_port, (err) => {
 app.use('/users', userController);
 app.use('/topics', topicController);
 app.use('/posts', postsRoute);
+app.use(fileHandler);
 
-app.post('/images', upload.single('image'), function(req, res, next) {
-    console.log("Upload successful.");
-    
-        res.json({
-            'success' : 1,
-            "file": {
-                "url": "http://3.34.76.182:3300/uploads/" + filename
-            }
-        })
-    
-});
-
-app.post('/images/url', (req, res, next) => {
-    
-    // Download image to server
-    download.image({
-        url: req.body.url,
-        dest: options.dest
-    })
-        .then(({ filename }) => {
-            console.log('Saved to ', filename)
-            res.json({ 
-                success: 1,
-                file: {
-                    url : "http://3.34.76.182:3300/uploads/" + (filename.slice(8, filename.length))
-                }
-             })
-        })
-        .catch((err) => {console.error(err); res.json({error: err}, {success: 0})})
-
-        
-} );
